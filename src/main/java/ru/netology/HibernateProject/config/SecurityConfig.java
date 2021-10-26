c9fa3e5e95d872df77e078d081fdb5ce06545172
VladKorchik <80666885+VladKorchik@users.noreply.github.com>
2021-10-26 21:27:10 +0300

    Security changed

--- src/main/java/ru/netology/HibernateProject/config/SecurityConfig.java
+++ src/main/java/ru/netology/HibernateProject/config/SecurityConfig.java
@@ -2,23 +2,26 @@ package ru.netology.HibernateProject.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("personsGetter").password(encoder.encode("1234")).roles("READ")
+                .and().withUser("admin").password(encoder.encode("123456")).roles("DELETE")
+                .and().withUser("preAdmin").password(encoder.encode("456789")).roles("WRITE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/index").permitAll()
-                .and().authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests().anyRequest().authenticated()
                 .and().formLogin();
     }
 }
--- src/main/java/ru/netology/HibernateProject/cotroller/GetController.java
+++ src/main/java/ru/netology/HibernateProject/cotroller/GetController.java
@@ -1,5 +1,7 @@
 package ru.netology.HibernateProject.cotroller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +24,21 @@ public class GetController {
     }
 
     @GetMapping("/get-by-age")
+    @Secured("ROLE_READ")
     public List<Person> getByAgeWhereAgeSmallerThan(@RequestParam("age") int age) {
         List<Person> result = getService.getByAgeWhereAgeSmallerThan(age);
         return result;
     }
 
     @GetMapping("/get-by-city")
+    @Secured("ROLE_READ")
     public List<Person> getByCity(@RequestParam("city") String city) {
         List<Person> result = getService.getByCity(city);
         return result;
     }
 
     @GetMapping("/createPerson")
+    @Secured({"ROLE_READ", "ROLE_WRITE"})
     public String addPerson(@RequestParam("name") String name, @RequestParam ("surname") String surname,
     @RequestParam ("age") int age, @RequestParam ("phonenumber") Integer phoneNumber,
                             @RequestParam ("city") String city ){
@@ -43,13 +48,15 @@ public class GetController {
     }
 
     @GetMapping ("/getPersonByNameAndSurname")
-    public String  getPersonByNameAndSurname(@RequestParam("name") String name,
+    @PreAuthorize("#username.equals('personsGetter')")
+    public String  getPersonByNameAndSurname(@RequestParam("username") String username,
                                              @RequestParam("surname") String surname) {
-        Person person = getService.getPersonByNameAndSurname(name,surname);
+        Person person = getService.getPersonByNameAndSurname(username,surname);
         return person.toString();
     }
 
     @GetMapping("/delete-person")
+    @PreAuthorize("hasRole('ROLE_DELETE') or hasRole(ROLE_WRITE)")
     public String deletePerson (@RequestParam("name") String name, @RequestParam("surname") String surname) {
         getService.deletePerson(name, surname);
         return new String("done");
